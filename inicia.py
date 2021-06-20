e503ed67c5f8b759aba0cec001bf4465f4a078a9
Matheus Rodrigues <matheusrodriguesh1@gmail.com>
2021-06-20 19:05:18 -0300

    testando o servidor gevent WSGIServer

--- inicia.py
+++ inicia.py
@@ -1,4 +1,9 @@
+# from app import api
+#
+# if __name__ == "__main__":
+#     api.run(host='192.168.0.7', port=58000, debug=True)
+from gevent.pywsgi import WSGIServer
 from app import api
 
-if __name__ == "__main__":
-    api.run(host='192.168.0.7', port=58000, debug=True)
+http_server = WSGIServer(('192.168.0.8', 5000), api)
+http_server.serve_forever()
